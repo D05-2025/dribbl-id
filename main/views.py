389eeadd380c4a617cb6445f0356f3b0387656b7
Khawarizmi14 <khawaydin@gmail.com>
2025-10-22 15:43:55 +0700

    add home app

---
+++ main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def show_main(request):
+    context = {
+        "title": "Main Page",
+        "kelompok" : "D05"
+    }
+    return render(request, "main.html", context)
