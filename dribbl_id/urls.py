6c2b982f637e2c60c74ef599e725d5e4d416a266
Khawarizmi14 <khawaydin@gmail.com>
2025-10-22 16:03:42 +0700

    update urls

--- dribbl_id/urls.py
+++ dribbl_id/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('news/', include('news.urls')),  
     path('auth/', include('authentication.urls')),
-    path('', include('main.urls')),
+    path('', include('home.urls')),
 ]
