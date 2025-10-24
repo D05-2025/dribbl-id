9c11693fc350c6fa836245687e2233668760434f
bimanyuw <f.abimanyuwijanarko@gmail.com>
2025-10-24 22:35:17 +0700

    Add default password for production database

--- dribbl_id/settings.py
+++ dribbl_id/settings.py
@@ -96,7 +96,7 @@ if PRODUCTION:
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.getenv('DB_NAME'),
             'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'PASSWORD': os.getenv('DB_PASSWORD', 'DUmLgfOp'),
             'HOST': os.getenv('DB_HOST', '152.118.29.139'),
             'PORT': os.getenv('DB_PORT', '5432'),
             'OPTIONS': {
