71132dcab98000cf7a9930553e95dc0fb9f34efe
rayayayya <raynabalq.s@gmail.com>
2025-12-20 19:05:55 +0700

    Test

--- news/urls.py
+++ news/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from news.views import show_json, show_json_by_id, add_news_entry_ajax, edit_news_entry_ajax, delete_news, show_news_page, show_news_detail, get_news_json, show_xml, show_xml_by_id, add_news_flutter
+from news.views import show_json, show_json_by_id, add_news_entry_ajax, edit_news_entry_ajax, delete_news, show_news_page, show_news_detail, get_news_json, show_xml, show_xml_by_id, add_news_flutter, proxy_image
 
 app_name = 'news'
 urlpatterns = [
     path('', show_news_page, name='show_news_page'),
     path('create-flutter/', add_news_flutter, name='create_news_flutter'),
-
+    path('proxy-image/', proxy_image, name='proxy_image'),
     path('json/', show_json, name='show_json'),
     path('json/<uuid:news_id>/', show_json_by_id, name='show_json_by_id'),
     path('get-news-json/<uuid:id>/', get_news_json, name='get_news_json'),
